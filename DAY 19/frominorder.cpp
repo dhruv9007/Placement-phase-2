9633f91a79dcc6302a21bbed6e662c342d29422d
dhruv9007 <108949075+dhruv9007@users.noreply.github.com>
2022-07-29 22:27:02 +0530

    Update frominorder.cpp

--- DAY 19/frominorder.cpp
+++ DAY 19/frominorder.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    TreeNode*helper(int preS, int preE, int inS,int inE,vector<int>& preorder, vector<int>& inorder,unordered_map<int,int> &mp)
-    {
-      if(preS>preE) return NULL;
-        
-        TreeNode*root=new TreeNode(preorder[preS]);
-        if(preS==preE) return root;
-        
-        int rootindex=mp[preorder[preS]];
-        int lpreS=preS+1;
-        int rpreE=preE;
-        int linS=inS;
-        int linE=rootindex-1;
-        int rinS=rootindex+1;
-        int rinE=inE;
-        int lpreE=linE-linS+lpreS;
-        int rpreS=lpreE+1;
-        
-        TreeNode*L=helper(lpreS,lpreE,linS,linE,preorder,inorder,mp);
-        TreeNode*R=helper(rpreS,rpreE,rinS,rinE,preorder,inorder,mp);
-        
-        root->left=L; root->right=R;
-        return root;
-    }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-       unordered_map<int,int> mp;
-        for(int i=0;i<inorder.size();i++)
-        {
-          mp[inorder[i]]=i;  
-        }
-        return helper(0,preorder.size()-1,0,inorder.size()-1,preorder,inorder,mp);
-    }
+	TreeNode* makeTree(vector<int>& post, int pS, int pE, vector<int>& in, int iS, int iE, unordered_map<int,int>& mp)
+	{
+		if(iS>iE || pS>pE)  return NULL;
+		TreeNode *root = new TreeNode(post[pE]);
+		int inRoot = mp[root->val];
+		int left = inRoot-iS;
+		root->left = makeTree(post,pS,pS+left-1,in,iS,inRoot-1,mp);
+		root->right = makeTree(post,pS+left,pE-1,in,inRoot+1,iE,mp);
+		return root;
+	}
+	TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+		unordered_map<int,int> mp;
+		for(int i=0;i<inorder.size();i++)   mp[inorder[i]] = i;
+		return makeTree(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1,mp);
+	}
 };
